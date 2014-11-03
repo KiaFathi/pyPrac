581788b34dfe1cff0cb1835e69f8d04e5a603bff
Kia Fathi <kiafathi@me.com>
2014-11-03 11:54:49 -0800

    (feat) complete ex1-7

---
+++ exs/ex1.py
@@ -0,0 +1,6 @@
+print "Hello World"
+print "Hello World Again"
+print "I love typing so much."
+print "This is so much fun."
+print "I\'d much rather have a dance 'party'"
+print 'I "said" do not print this?'
